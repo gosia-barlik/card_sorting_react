922ac945c7db058d66e0c31c0adb702d9d18e99c
Malgorzata <malgorzata.barlik@gmail.com>
2021-10-06 16:25:29 +0200

    delete file

--- src/components/GraphList.jsx
+++ src/components/GraphList.jsx
@@ -4,9 +4,10 @@ import { NavLink } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from '@mui/material/CardMedia';
+import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 export default function GraphList() {
   const [graphs, setGraphs] = useState([]);
@@ -56,43 +57,82 @@ export default function GraphList() {
     });
   };
 
-  return (
-    <div style={{display:'flex', flexDirection:'column', marginLeft: "10%" }}>
-    <div style={{marginLeft: "20%"}}>
-    <Typography style={{textAlign:'left', padding: '20px', fontWeight: '600'}}>Collections:</Typography>
-    </div>
-    <div style={{ marginLeft: "20%", display: "flex" }}>
-      {graphs &&
-        graphs.map((graph) => {
-          return (
-            <Card
-              sx={{ minWidth: 275 }}
-              style={{ margin: "20px", height: "150px" }}>
-              <CardContent>
-                <NavLink
-                  to={`/Graph/${graph.id}`}
-                  style={{ fontSize: "12px", color: "rgb(42, 41, 41)" }}>
-                  <li style={{listStyle:'none'}} key={graph.id}>name: <strong> {graph.name} </strong></li>
-                </NavLink>
-              </CardContent>
-            </Card>
-          );
-        })}
+  const deleteFile = (id) => {
+    console.log(id);
+    db.collection("professions").doc(id).delete()
+  };
 
-      <Card sx={{ minWidth: 275 }} style={{ margin: "20px", height: "150px" }}>
-        <CardContent style={{ paddingBottom: "20px"}}>
-          Upload Files
-          <br />
-          <input type='file' id='files' class='hidden' />
-          <label for='files'>Select file</label>
-          <br />
-          <button id='send'>
-            Upload
-          </button>
-        </CardContent >
-      </Card>
-   
-    </div>
-    </div>
+  return (
+    <Container
+      maxWidth='m'
+      style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ marginLeft: "20%" }}>
+        <Typography
+          style={{
+            textAlign: "left",
+            padding: "20px 0 0 20px",
+            fontWeight: "600",
+          }}>
+          Documents:
+        </Typography>
+      </div>
+      <div style={{ marginLeft: "20%", display: "flex" }}>
+        {graphs &&
+          graphs.map((graph) => {
+            return (
+              <Card
+                sx={{ minWidth: 275 }}
+                style={{ margin: "20px", height: "150px" }}>
+                <CardContent>
+                  <NavLink
+                    to={`/Graph/${graph.id}`}
+                    style={{
+                      fontSize: "12px",
+                      color: "rgb(42, 41, 41)",
+                      marginBottom: "5px",
+                    }}>
+                    <li style={{ listStyle: "none" }} key={graph.id}>
+                      name: <strong> {graph.name} </strong>
+                    </li>
+                  </NavLink>
+                  <div
+                    style={{
+                      margin: "70px 15px 0 15px",
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}>
+                    <NavLink
+                      to={`/Graph/${graph.id}`}
+                      style={{ fontSize: "12px", color: "rgb(52, 51, 51)" }}>
+                      EDIT
+                    </NavLink>
+                    <br></br>
+                    <button
+                      id='delete'
+                      onClick={()=> deleteFile(`${graph.id}`)}
+                      style={{ fontSize: "12px", color: "rgb(42, 41, 41)" }}>
+                      DELETE
+                    </button>
+                  </div>
+                </CardContent>
+              </Card>
+            );
+          })}
+
+        <Card
+          sx={{ minWidth: 275 }}
+          style={{ margin: "20px", height: "150px", fontSize: "12px" }}>
+          <CardContent style={{}}>
+            <strong>Upload Files</strong>
+            <br />
+            <input type='file' id='files' class='hidden' />
+            <label for='files'>Select file</label>
+            <br />
+            <button id='send'>Upload</button>
+          </CardContent>
+        </Card>
+      </div>
+    </Container>
   );
 }
